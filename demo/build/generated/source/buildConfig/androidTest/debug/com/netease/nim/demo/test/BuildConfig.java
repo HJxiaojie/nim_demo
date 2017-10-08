a5100c1018c9720dfc593b2370a6b6382f5deccd
jie <923690997@qq.com>
2017-10-08 22:51:12 +0800

    new

--- demo/build/generated/source/buildConfig/androidTest/debug/com/netease/nim/demo/test/BuildConfig.java
+++ demo/build/generated/source/buildConfig/androidTest/debug/com/netease/nim/demo/test/BuildConfig.java
@@ -11,6 +11,6 @@ public final class BuildConfig {
   public static final int VERSION_CODE = -1;
   public static final String VERSION_NAME = "";
   // Fields from default config.
-  public static final String BUILD_DATE = "2017-10-8 17:38:14";
-  public static final String GIT_REVISION = "";
+  public static final String BUILD_DATE = "2017-10-8 22:46:48";
+  public static final String GIT_REVISION = "ca787cab";
 }
--- demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
+++ demo/build/generated/source/buildConfig/debug/com/netease/nim/demo/BuildConfig.java
@@ -11,6 +11,6 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 38;
   public static final String VERSION_NAME = "4.2.0";
   // Fields from default config.
-  public static final String BUILD_DATE = "2017-10-8 17:38:14";
-  public static final String GIT_REVISION = "";
+  public static final String BUILD_DATE = "2017-10-8 22:47:53";
+  public static final String GIT_REVISION = "ca787cab";
 }
